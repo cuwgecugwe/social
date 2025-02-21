9cc2764dacd02e7feda817392570e3373389123c
cuwgecugwe <manuelfloreshermoza@gmail.com>
2025-02-21 11:02:07 -0500

    02

--- database/migrations/2025_02_12_021743_create_proyectos_table.php
+++ database/migrations/2025_02_12_021743_create_proyectos_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('descripcion');
             $table->string('fecha');
             $table->string('hora');
+            $table->string('imagen');
             $table->timestamps();
         });
     }
